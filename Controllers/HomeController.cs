0f9f79545025acc4685e47c91d77762697e65896
Simon-Boyer <si.boyer@hotmail.ca>
2024-06-16 21:18:59 -0400

    cleanup

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,89 +8,15 @@ namespace dotnet_otel.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly RequestMeter _requestMeter;
-    public ActivitySource activitySource;
 
-    public HomeController(ILogger<HomeController> logger, Instrumentation instrumentation, RequestMeter requestMeter)
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-        _requestMeter = requestMeter;
-        activitySource = instrumentation.ActivitySource;
     }
 
     public IActionResult Index()
     {
         return View();
-    }
-
-    private void TestSpans() {
-        using var activity1 = activitySource.StartActivity("test.1");
-        // _logger.LogInformation("Test level 1");
-
-        using var activity2 = activitySource.StartActivity("test.2").SetParentId(activity1.TraceId, activity1.SpanId);
-        // _logger.LogInformation("Test level 2");
-    }
-
-    public IActionResult Test()
-    {
-        using (var activity0 = activitySource.StartActivity("test.a")) {
-            _logger.LogInformation("Test A");
-
-            using (var activity1 = activitySource.StartActivity("test.b")) {
-                _logger.LogInformation("Test B");
-
-                TestSpans();
-            }
-        }
-        return Ok("Test OK");
-    }
-
-    public IActionResult Count()
-    {
-        _requestMeter.RequestCounter.Add(1, new KeyValuePair<string, object?>("ip", Request.HttpContext.Connection.RemoteIpAddress.ToString()));
-        return Ok("OK");
-    }
-
-    public async Task<IActionResult> MakeRequest([FromQuery(Name = "code")] string code)
-    {
-        using (var myActivity = activitySource.StartActivity("MakeRequest"))
-        {
-            if(code == "" || code is null) code = "0";
-            myActivity.SetTag("code", code);
-            myActivity.SetTag("http.method", "GET");
-
-            int intCode;
-            try
-            {
-                intCode = int.Parse(code);
-            }
-            catch (Exception ex)
-            {
-                myActivity.SetStatus(ActivityStatusCode.Error, "User did not give a number as a code");
-                myActivity.RecordException(ex);
-                return BadRequest("Code must be a number");
-            }
-
-            var http = new HttpClient();
-
-            if (intCode == 0) {
-                http.BaseAddress = new Uri("http://127.0.0.1:5120");
-                var responseTask = http.GetAsync("/Home/MakeRequest?code=200");
-                myActivity.AddEvent(new("request.sent"));
-                using var response = await responseTask;
-                myActivity.AddEvent(new("request.completed"));
-
-                return Ok($"Response code: {response.StatusCode}\nMessage: {await response.Content.ReadAsStringAsync()}");
-            } else {
-                http.BaseAddress = new Uri("https://httpstat.us");
-                var responseTask = http.GetAsync(intCode.ToString());
-                myActivity.AddEvent(new("request.sent"));
-                using var response = await responseTask;
-                myActivity.AddEvent(new("request.completed"));
-
-                return Ok($"Response code: {response.StatusCode}\nMessage: {await response.Content.ReadAsStringAsync()}");
-            }
-        }
     }
 
     public IActionResult Privacy()
--- Program.cs
+++ Program.cs
@@ -22,10 +22,6 @@ services.Configure<ForwardedHeadersOptions>(options =>
         ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 });
 
-var MyInstrumentation = new Instrumentation();
-builder.Services.AddSingleton<Instrumentation>(MyInstrumentation);
-ActivitySource MyActivitySource = MyInstrumentation.ActivitySource;
-
 var configBuilder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -37,93 +33,6 @@ IConfiguration configuration = configBuilder.Build();
 // Add services to the container.
 services.AddControllersWithViews();
 
-// Load Dynatrace Metadata
-List<KeyValuePair<string, object>> metadata = new List<KeyValuePair<string, object>>();
-foreach (string name in configuration.GetSection("Otlp:MetadataFiles").Get<string[]>())
-{
-    try
-    {
-        foreach (string line in System.IO.File.ReadAllLines(name.StartsWith("/var") ? name : System.IO.File.ReadAllText(name)))
-        {
-            var keyvalue = line.Split("=");
-            metadata.Add(new KeyValuePair<string, object>(keyvalue[0], keyvalue[1]));
-        }
-    }
-    catch { }
-}
-
-Action<ResourceBuilder> configureResource = r => r
-    .AddService(serviceName: MyActivitySource.Name)
-    .AddAttributes(metadata);
-
-// Add OTEL services
-services.AddOpenTelemetry()
-    .ConfigureResource(configureResource)
-    .WithTracing(builder => { 
-        builder.SetSampler(new AlwaysOnSampler());
-        builder.AddSource(MyActivitySource.Name);
-        builder.AddAspNetCoreInstrumentation();
-        if (configuration.GetValue<string>("Otlp:TracesEndpoint") != "") {
-            builder.AddOtlpExporter(options =>
-            {
-                options.Endpoint = new Uri(configuration.GetValue<string>("Otlp:TracesEndpoint")); 
-                options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
-                options.Headers = configuration.GetValue<string>("Otlp:Headers");
-                options.ExportProcessorType = OpenTelemetry.ExportProcessorType.Simple;
-            });
-        }
-    })
-    .WithMetrics(builder => {
-        builder.AddMeter(RequestMeter.MeterName);
-        if (configuration.GetValue<string>("Otlp:MetricsEndpoint") != "") {
-            builder.AddOtlpExporter((OtlpExporterOptions exporterOptions, MetricReaderOptions readerOptions) =>
-            {
-                exporterOptions.Endpoint = new Uri(configuration.GetValue<string>("Otlp:MetricsEndpoint"));
-                exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
-                exporterOptions.Headers = configuration.GetValue<string>("Otlp:Headers");
-                readerOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta;
-            });
-        }
-    });
-
-var resourceBuilder = ResourceBuilder.CreateDefault();
-configureResource!(resourceBuilder);
-
-Sdk.CreateTracerProviderBuilder()
-    .SetSampler(new AlwaysOnSampler())
-    .AddSource(MyActivitySource.Name)
-    .ConfigureResource(configureResource);
-
-services.AddSingleton<RequestMeter>();
-
-// Configure OTEL logger
-loggerFactoryOT = LoggerFactory.Create(builder =>
-{
-   builder
-       .AddOpenTelemetry(builder =>
-       {
-           builder.SetResourceBuilder(resourceBuilder);
-           if (configuration.GetValue<string>("Otlp:LogsEndpoint") != ""){
-            builder.AddOtlpExporter(options =>
-            {
-                options.Endpoint = new Uri(configuration.GetValue<string>("Otlp:LogsEndpoint"));
-                options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
-                options.Headers = configuration.GetValue<string>("Otlp:Headers");
-                options.ExportProcessorType = OpenTelemetry.ExportProcessorType.Batch;
-            });
-           }
-       })
-       .AddConsole();
-});
-
-
-var logger = loggerFactoryOT.CreateLogger<Program>();
-services.AddSingleton<ILoggerFactory>(loggerFactoryOT);
-// services.AddSingleton(logger);
-
-
-// Test logger
-logger.LogInformation(eventId: 123, "OtelTest");
 
 var app = builder.Build();
 
